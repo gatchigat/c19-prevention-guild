7e0a9a14cc1ed5fec28fa84fbd98464a7643c58e
gatchigat <80865346+gatchigat@users.noreply.github.com>
2022-04-17 01:03:57 +0800

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -83,3 +83,28 @@ $(function () {
     $(modalId).modal("show");
   });
 });
+// JS for Back to top button
+mybutton = document.getElementById("myBtn");
+
+// When the user scrolls down 500px from the top of the document, show the button
+window.onscroll = function () {
+  scrollFunction();
+};
+
+//PAGE HEIGHT WHEN THE BUTTON APPEARS IS SET AT 700px
+function scrollFunction() {
+  if (
+    document.body.scrollTop > 300 ||
+    document.documentElement.scrollTop > 300
+  ) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+}
